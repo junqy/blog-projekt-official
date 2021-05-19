fc823f4d77a249e739d6b9ba0c5c4e23eeb6fc38
junqy <popek1814@gmail.com>
2021-05-19 02:26:44 +0200

    Użytkownicy, logowanie, rejestracja, admin, poprawa mainpage

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,21 @@
-class RegistrationsController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user=User.new(user_params)
-    if @user.save
-      redirect_to root_path, notice: "Rejestracja zakończona sukcesem"
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :login, :nickname, :password, :password_confirmation)
-  end
-end
-
+# class RegistrationsController < ApplicationController
+#   def new
+#     @user = User.new
+#   end
+#
+#   def create
+#     @user=User.new(user_params)
+#     if @user.save
+#       redirect_to root_path, notice: "Rejestracja zakończona sukcesem"
+#     else
+#       render :new
+#     end
+#   end
+#
+#   private
+#
+#   def user_params
+#     params.require(:user).permit(:email, :password, :password_confirmation)
+#   end
+# end
+#
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_one :blog
   has_many :comments
-  has_secure_password
   validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "nieprawidłowy adres e-mail" }
-  validates :login, presence: true
-  validates :nickname, presence: true
+  # validates :login, presence: true
+  # validates :nickname, presence: true
 
 end
